97ac6d5770af9a7992be3e850488b43f037a7dfe
noko206 <61347127+noko206@users.noreply.github.com>
2023-05-25 09:17:06 +0900

    add: 20230525あさかつバチャ

---
+++ morning/20230525/4.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+#include <atcoder/all>
+using namespace atcoder;
+#endif
+
+#define _overload3(_1,_2,_3,name,...) name
+#define _REP(i,n) REPI(i,0,n)
+#define REPI(i,a,b) for(int i=int(a);i<int(b);++i)
+#define REP(...) _overload3(__VA_ARGS__,REPI,_REP,)(__VA_ARGS__)
+#define _RREP(i,n) RREPI(i,n,0)
+#define RREPI(i,a,b) for(int i=int(a);i>=int(b);--i)
+#define RREP(...) _overload3(__VA_ARGS__,RREPI,_RREP,)(__VA_ARGS__)
+#define ALL(a) (a).begin(),(a).end()
+#define ALLR(a) (a).rbegin(),(a).rend()
+typedef long long ll;
+const int INF32 = 1001001001;
+const long long INF64 = 1001001001001001001;
+struct Init { Init() { ios::sync_with_stdio(0); cin.tie(0); }} init;
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T, class... Ts> void output(const T& a, const Ts&... b) { cout << a; (cout << ... << (cout << ' ', b)); cout << '\n'; }
+template<class T> void output(vector<T> v) { for (auto u : v) cout << u << ' '; cout << '\n'; };
+void yesno(bool is_ok) { cout << (is_ok ? "yes" : "no") << '\n'; }
+void YesNo(bool is_ok) { cout << (is_ok ? "Yes" : "No") << '\n'; }
+void YESNO(bool is_ok) { cout << (is_ok ? "YES" : "NO") << '\n'; }
+
+// vector<ll> divisors(ll n){
+// 	vector<ll> ans;
+// 	for(ll i = 1; i * i <= n; ++i){
+// 		if(n % i == 0){
+// 			ans.push_back(i);
+// 			if(i != n / i){
+// 				ans.push_back(n / i);
+// 			}
+// 		}
+// 	}
+// 	sort(ALL(ans));
+// 	return ans;
+// }
+
+int main(){
+	ll n, m;
+	cin >> n >> m;
+	ll k = (int)sqrt(m) + 5;
+	ll ans = INF64;
+	REP(i, 1, min(k, n) + 1){
+		ll j = (m + i - 1) / i;
+		if(j <= n) chmin(ans, i * j);
+	}
+	if(ans == INF64) output(-1);
+	else output(ans);
+}
