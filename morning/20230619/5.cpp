5c4fdf32f9ea43c80b3f855d4c92cafcd74dee94
noko206 <61347127+noko206@users.noreply.github.com>
2023-06-19 09:31:01 +0900

    add: 20230619あさかつバチャ

---
+++ morning/20230619/5.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+#include <atcoder/all>
+using namespace atcoder;
+#endif
+
+#define _overload3(_1,_2,_3,name,...) name
+#define _REP(i,n) REPI(i,0,n)
+#define REPI(i,a,b) for(int i=int(a);i<int(b);++i)
+#define REP(...) _overload3(__VA_ARGS__,REPI,_REP,)(__VA_ARGS__)
+#define _RREP(i,n) RREPI(i,n,0)
+#define RREPI(i,a,b) for(int i=int(a);i>=int(b);--i)
+#define RREP(...) _overload3(__VA_ARGS__,RREPI,_RREP,)(__VA_ARGS__)
+#define ALL(a) (a).begin(),(a).end()
+#define ALLR(a) (a).rbegin(),(a).rend()
+typedef long long ll;
+const int INF32 = 1001001001;
+const long long INF64 = 1001001001001001001;
+struct Init { Init() { ios::sync_with_stdio(0); cin.tie(0); }} init;
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T, class... Ts> void output(const T& a, const Ts&... b) { cout << a; (cout << ... << (cout << ' ', b)); cout << '\n'; }
+template<class T> void output(vector<T> v) { for (auto u : v) cout << u << ' '; cout << '\n'; };
+void yesno(bool is_ok) { cout << (is_ok ? "yes" : "no") << '\n'; }
+void YesNo(bool is_ok) { cout << (is_ok ? "Yes" : "No") << '\n'; }
+void YESNO(bool is_ok) { cout << (is_ok ? "YES" : "NO") << '\n'; }
+
+using mint = modint1000000007;
+
+int main(){
+	int n;
+	cin >> n;
+	vector<ll> a(n);
+	REP(i, n) cin >> a[i];
+	vector<vector<int>> cnt(n + 1, vector<int>(60, 0)); // 1の個数
+	mint ans = 0;
+	int mod = 1000000007;
+	REP(i, n){
+		REP(j, 60){
+			if(a[i] & (1LL << j)){
+				ans += ((1LL << j) % mod) * (i - cnt[i][j]);
+				++cnt[i + 1][j];
+			} else {
+				ans += ((1LL << j) % mod) * cnt[i][j];
+			}
+			cnt[i + 1][j] += cnt[i][j];
+		}
+	}
+	output(ans.val());
+}
