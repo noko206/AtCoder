4afb8fa02cc9570752ae2bb38e526effe3143e20
noko206 <61347127+noko206@users.noreply.github.com>
2023-06-27 09:11:03 +0900

    add: 20230627あさかつバチャ

---
+++ morning/20230627/6.cpp
@@ -0,0 +1,58 @@
+// clang-format off
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+#include <atcoder/all>
+using namespace atcoder;
+#endif
+
+#define _overload3(_1,_2,_3,name,...) name
+#define _REP(i,n) REPI(i,0,n)
+#define REPI(i,a,b) for(int i=int(a);i<int(b);++i)
+#define REP(...) _overload3(__VA_ARGS__,REPI,_REP,)(__VA_ARGS__)
+#define _RREP(i,n) RREPI(i,n,0)
+#define RREPI(i,a,b) for(int i=int(a);i>=int(b);--i)
+#define RREP(...) _overload3(__VA_ARGS__,RREPI,_RREP,)(__VA_ARGS__)
+#define ALL(a) (a).begin(),(a).end()
+#define ALLR(a) (a).rbegin(),(a).rend()
+typedef long long ll;
+const int INF32 = 1001001001;
+const long long INF64 = 1001001001001001001;
+struct Init { Init() { ios::sync_with_stdio(0); cin.tie(0); }} init;
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T, class... Ts> void output(const T& a, const Ts&... b) { cout << a; (cout << ... << (cout << ' ', b)); cout << '\n'; }
+template<class T> void output(vector<T> v) { for (auto u : v) cout << u << ' '; cout << '\n'; };
+void yesno(bool is_ok) { cout << (is_ok ? "yes" : "no") << '\n'; }
+void YesNo(bool is_ok) { cout << (is_ok ? "Yes" : "No") << '\n'; }
+void YESNO(bool is_ok) { cout << (is_ok ? "YES" : "NO") << '\n'; }
+
+// clang-format on
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> to(n);
+    REP(i, m) {
+        int u, v;
+        cin >> u >> v;
+        --u, --v;
+        to[u].push_back(v);
+    }
+    int s, t;
+    cin >> s >> t;
+    --s, --t;
+    vector<vector<int>> dist(n, vector<int>(3, INF32));
+    dist[s][0] = 0;
+    queue<pair<int, int>> q;
+    q.push({s, 0});
+    while (!q.empty()) {
+        auto [v, cnt] = q.front();
+        q.pop();
+        for (auto u : to[v]) {
+            if (chmin(dist[u][(cnt + 1) % 3], dist[v][cnt] + 1)) {
+                q.push({u, (cnt + 1) % 3});
+            }
+        }
+    }
+    output(dist[t][0] == INF32 ? -1 : dist[t][0] / 3);
+}
