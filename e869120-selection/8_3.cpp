640cd1c26c71dcab5d2614e2de060b9f98fe4cc4
noko206 <61347127+noko206@users.noreply.github.com>
2023-07-05 00:03:30 +0900

    add: 精進

---
+++ e869120-selection/8_3.cpp
@@ -0,0 +1,50 @@
+// clang-format off
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+#include <atcoder/all>
+using namespace atcoder;
+#endif
+
+#define _overload3(_1,_2,_3,name,...) name
+#define _REP(i,n) REPI(i,0,n)
+#define REPI(i,a,b) for(int i=int(a);i<int(b);++i)
+#define REP(...) _overload3(__VA_ARGS__,REPI,_REP,)(__VA_ARGS__)
+#define _RREP(i,n) RREPI(i,n,0)
+#define RREPI(i,a,b) for(int i=int(a);i>=int(b);--i)
+#define RREP(...) _overload3(__VA_ARGS__,RREPI,_RREP,)(__VA_ARGS__)
+#define ALL(a) (a).begin(),(a).end()
+#define ALLR(a) (a).rbegin(),(a).rend()
+typedef long long ll;
+const int INF32 = 1001001001;
+const long long INF64 = 1001001001001001001;
+struct Init { Init() { ios::sync_with_stdio(0); cin.tie(0); }} init;
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T, class... Ts> void output(const T& a, const Ts&... b) { cout << a; (cout << ... << (cout << ' ', b)); cout << '\n'; }
+template<class T> void output(vector<T> v) { for (auto u : v) cout << u << ' '; cout << '\n'; };
+void yesno(bool is_ok) { cout << (is_ok ? "yes" : "no") << '\n'; }
+void YesNo(bool is_ok) { cout << (is_ok ? "Yes" : "No") << '\n'; }
+void YESNO(bool is_ok) { cout << (is_ok ? "YES" : "NO") << '\n'; }
+
+// clang-format on
+int main() {
+    int n;
+    cin >> n;
+    vector<ll> a(n), b(n);
+    REP(i, n) cin >> a[i] >> b[i];
+    vector<ll> sorted_a = a, sorted_b = b;
+    sort(ALL(sorted_a));
+    sort(ALL(sorted_b));
+    ll median_a = n % 2 == 0 ? (sorted_a[n / 2] + sorted_a[n / 2 - 1] + 1) / 2
+                             : sorted_a[n / 2];
+    ll median_b = n % 2 == 0 ? (sorted_b[n / 2] + sorted_b[n / 2 - 1] + 1) / 2
+                             : sorted_b[n / 2];
+    ll ans = 0;
+    REP(i, n) {
+        ans += abs(a[i] - b[i]);
+        ans += abs(median_a - a[i]);
+        ans += abs(median_b - b[i]);
+    }
+    output(ans);
+}
