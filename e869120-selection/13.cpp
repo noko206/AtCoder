b432173600af184e1f87fd245ea448a55a6664f1
noko206 <61347127+noko206@users.noreply.github.com>
2023-07-06 20:44:46 +0900

    add: 精進

---
+++ e869120-selection/13.cpp
@@ -0,0 +1,52 @@
+// clang-format off
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+#include <atcoder/all>
+using namespace atcoder;
+#endif
+
+#define _overload3(_1,_2,_3,name,...) name
+#define _REP(i,n) REPI(i,0,n)
+#define REPI(i,a,b) for(int i=int(a);i<int(b);++i)
+#define REP(...) _overload3(__VA_ARGS__,REPI,_REP,)(__VA_ARGS__)
+#define _RREP(i,n) RREPI(i,n,0)
+#define RREPI(i,a,b) for(int i=int(a);i>=int(b);--i)
+#define RREP(...) _overload3(__VA_ARGS__,RREPI,_RREP,)(__VA_ARGS__)
+#define ALL(a) (a).begin(),(a).end()
+#define ALLR(a) (a).rbegin(),(a).rend()
+typedef long long ll;
+const int INF32 = 1001001001;
+const long long INF64 = 1001001001001001001;
+struct Init { Init() { ios::sync_with_stdio(0); cin.tie(0); }} init;
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T, class... Ts> void output(const T& a, const Ts&... b) { cout << a; (cout << ... << (cout << ' ', b)); cout << '\n'; }
+template<class T> void output(vector<T> v) { for (auto u : v) cout << u << ' '; cout << '\n'; };
+void yesno(bool is_ok) { cout << (is_ok ? "yes" : "no") << '\n'; }
+void YesNo(bool is_ok) { cout << (is_ok ? "Yes" : "No") << '\n'; }
+void YESNO(bool is_ok) { cout << (is_ok ? "YES" : "NO") << '\n'; }
+
+// clang-format on
+int main() {
+    int r, c;
+    cin >> r >> c;
+    vector a(r, vector<int>(c));
+    REP(i, r) REP(j, c) cin >> a[i][j];
+    vector<int> row(c);
+    REP(i, c) {
+        int tmp = 0;
+        REP(j, r) { tmp |= a[j][i] << j; }
+        row[i] = tmp;
+    }
+    int ans = 0;
+    REP(bit, 1 << r) {
+        int sum = 0;
+        REP(i, c) {
+            int cnt = __builtin_popcount(bit ^ row[i]);
+            sum += max(cnt, r - cnt);
+        }
+        chmax(ans, sum);
+    }
+    output(ans);
+}
