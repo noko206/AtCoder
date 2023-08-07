7c54d057c4a1c48bcd44366eeb996f85409c4e1f
noko206 <61347127+noko206@users.noreply.github.com>
2023-08-08 00:10:28 +0900

    add: 精進

---
+++ tessoku/4/a21.cpp
@@ -0,0 +1,48 @@
+// clang-format off
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+#include <atcoder/all>
+using namespace atcoder;
+#endif
+
+#define _overload3(_1,_2,_3,name,...) name
+#define _REP(i,n) REPI(i,0,n)
+#define REPI(i,a,b) for(int i=int(a);i<int(b);++i)
+#define REP(...) _overload3(__VA_ARGS__,REPI,_REP,)(__VA_ARGS__)
+#define _RREP(i,n) RREPI(i,n,0)
+#define RREPI(i,a,b) for(int i=int(a);i>=int(b);--i)
+#define RREP(...) _overload3(__VA_ARGS__,RREPI,_RREP,)(__VA_ARGS__)
+#define ALL(a) (a).begin(),(a).end()
+#define ALLR(a) (a).rbegin(),(a).rend()
+typedef long long ll;
+const int INF32 = 1001001001;
+const long long INF64 = 1001001001001001001;
+struct Init { Init() { ios::sync_with_stdio(0); cin.tie(0); cout << setprecision(15); }} init;
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> T gcd(T x, T y){ return (x % y) ? gcd(y, x % y) : y; }
+template<class T> T lcm(T x, T y){ return x / gcd(x, y) * y; }
+template<class T, class... Ts> void output(const T& a, const Ts&... b) { cout << a; (cout << ... << (cout << ' ', b)); cout << '\n'; }
+template<class T> void output(vector<T> v) { for (auto u : v) cout << u << ' '; cout << '\n'; };
+void yesno(bool is_ok) { cout << (is_ok ? "yes" : "no") << '\n'; }
+void YesNo(bool is_ok) { cout << (is_ok ? "Yes" : "No") << '\n'; }
+void YESNO(bool is_ok) { cout << (is_ok ? "YES" : "NO") << '\n'; }
+
+// clang-format on
+int main() {
+    int n;
+    cin >> n;
+    vector<int> p(n), a(n);
+    REP(i, n) cin >> p[i] >> a[i], --p[i];
+    vector dp(n, vector<int>(n, -1));
+    auto dfs = [&](auto &dfs, int l, int r) -> int {
+        if (l == r) return 0;
+        if (dp[l][r] != -1) return dp[l][r];
+        int ans1 = dfs(dfs, l + 1, r) + (l + 1 <= p[l] && p[l] <= r ? a[l] : 0);
+        int ans2 = dfs(dfs, l, r - 1) + (l <= p[r] && p[r] <= r - 1 ? a[r] : 0);
+        return dp[l][r] = max(ans1, ans2);
+    };
+    dfs(dfs, 0, n - 1);
+    output(dp[0][n - 1]);
+}
---
+++ tessoku/4/b23.cpp
@@ -0,0 +1,52 @@
+// clang-format off
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+#include <atcoder/all>
+using namespace atcoder;
+#endif
+
+#define _overload3(_1,_2,_3,name,...) name
+#define _REP(i,n) REPI(i,0,n)
+#define REPI(i,a,b) for(int i=int(a);i<int(b);++i)
+#define REP(...) _overload3(__VA_ARGS__,REPI,_REP,)(__VA_ARGS__)
+#define _RREP(i,n) RREPI(i,n,0)
+#define RREPI(i,a,b) for(int i=int(a);i>=int(b);--i)
+#define RREP(...) _overload3(__VA_ARGS__,RREPI,_RREP,)(__VA_ARGS__)
+#define ALL(a) (a).begin(),(a).end()
+#define ALLR(a) (a).rbegin(),(a).rend()
+typedef long long ll;
+const int INF32 = 1001001001;
+const long long INF64 = 1001001001001001001;
+struct Init { Init() { ios::sync_with_stdio(0); cin.tie(0); cout << setprecision(15); }} init;
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> T gcd(T x, T y){ return (x % y) ? gcd(y, x % y) : y; }
+template<class T> T lcm(T x, T y){ return x / gcd(x, y) * y; }
+template<class T, class... Ts> void output(const T& a, const Ts&... b) { cout << a; (cout << ... << (cout << ' ', b)); cout << '\n'; }
+template<class T> void output(vector<T> v) { for (auto u : v) cout << u << ' '; cout << '\n'; };
+void yesno(bool is_ok) { cout << (is_ok ? "yes" : "no") << '\n'; }
+void YesNo(bool is_ok) { cout << (is_ok ? "Yes" : "No") << '\n'; }
+void YESNO(bool is_ok) { cout << (is_ok ? "YES" : "NO") << '\n'; }
+
+// clang-format on
+int main() {
+    int n;
+    cin >> n;
+    vector<double> x(n), y(n);
+    REP(i, n) cin >> x[i] >> y[i];
+    vector dp(1 << n, vector<double>(n, (double)INF64));
+    dp[0][0] = 0.0;
+    REP(bit, 1 << n) {
+        REP(i, n) {
+            if (bit != 0 && !(bit & (1 << i))) continue;
+            if (dp[bit][i] >= (double)INF64) continue;
+            REP(j, n) {
+                if (bit & (1 << j)) continue;
+                double dist = sqrt(pow(x[i] - x[j], 2) + pow(y[i] - y[j], 2));
+                chmin(dp[bit | (1 << j)][j], dp[bit][i] + dist);
+            }
+        }
+    }
+    output(dp[(1 << n) - 1][0]);
+}
