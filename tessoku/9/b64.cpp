bbf47b058b3237cd32e137bda3284b74dea0e4c6
noko206 <61347127+noko206@users.noreply.github.com>
2023-08-12 20:57:09 +0900

    add: 鉄則 9章 グラフアルゴリズム 途中

---
+++ tessoku/9/b64.cpp
@@ -0,0 +1,74 @@
+// clang-format off
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+#include <atcoder/all>
+using namespace atcoder;
+#endif
+
+#define _overload3(_1,_2,_3,name,...) name
+#define _REP(i,n) REPI(i,0,n)
+#define REPI(i,a,b) for(int i=int(a);i<int(b);++i)
+#define REP(...) _overload3(__VA_ARGS__,REPI,_REP,)(__VA_ARGS__)
+#define _RREP(i,n) RREPI(i,n,0)
+#define RREPI(i,a,b) for(int i=int(a);i>=int(b);--i)
+#define RREP(...) _overload3(__VA_ARGS__,RREPI,_RREP,)(__VA_ARGS__)
+#define ALL(a) (a).begin(),(a).end()
+#define ALLR(a) (a).rbegin(),(a).rend()
+typedef long long ll;
+const int INF32 = 1001001001;
+const long long INF64 = 1001001001001001001;
+struct Init { Init() { ios::sync_with_stdio(0); cin.tie(0); cout << setprecision(15); }} init;
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> T gcd(T x, T y){ return (x % y) ? gcd(y, x % y) : y; }
+template<class T> T lcm(T x, T y){ return x / gcd(x, y) * y; }
+template<class T, class... Ts> void output(const T& a, const Ts&... b) { cout << a; (cout << ... << (cout << ' ', b)); cout << '\n'; }
+template<class T> void output(vector<T> v) { for (auto u : v) cout << u << ' '; cout << '\n'; };
+void yesno(bool is_ok) { cout << (is_ok ? "yes" : "no") << '\n'; }
+void YesNo(bool is_ok) { cout << (is_ok ? "Yes" : "No") << '\n'; }
+void YESNO(bool is_ok) { cout << (is_ok ? "YES" : "NO") << '\n'; }
+
+// clang-format on
+using P = pair<int, ll>;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<vector<P>> to(n);
+    REP(i, m) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        --a, --b;
+        to[a].emplace_back(c, b);
+        to[b].emplace_back(c, a);
+    }
+    priority_queue<P, vector<P>, greater<P>> pq;
+    pq.push({0, 0});
+    vector<ll> dist(n, INF64);
+    dist[0] = 0;
+    while (!pq.empty()) {
+        auto [d, v] = pq.top();
+        pq.pop();
+        if (dist[v] < d) continue;
+        for (auto [c, u] : to[v]) {
+            if (chmin(dist[u], dist[v] + c)) {
+                pq.push({dist[u], u});
+            }
+        }
+    }
+    vector<int> ans;
+    ans.push_back(n);
+    int v = n - 1;
+    while (v != 0) {
+        for (auto [c, u] : to[v]) {
+            if (dist[v] - c == dist[u]) {
+                ans.push_back(u + 1);
+                v = u;
+                break;
+            }
+        }
+    }
+    reverse(ALL(ans));
+    output(ans);
+}
