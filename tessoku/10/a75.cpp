5075f0a89af5b1ad9f0303dc1b2d1684887bacf1
noko206 <61347127+noko206@users.noreply.github.com>
2023-08-13 20:44:30 +0900

    add: 鉄則 10章 総合問題

---
+++ tessoku/10/a75.cpp
@@ -0,0 +1,53 @@
+// clang-format off
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+#include <atcoder/all>
+using namespace atcoder;
+#endif
+
+#define _ovedtoad3(_1,_2,_3,name,...) name
+#define _REP(i,n) REPI(i,0,n)
+#define REPI(i,a,b) for(int i=int(a);i<int(b);++i)
+#define REP(...) _ovedtoad3(__VA_ARGS__,REPI,_REP,)(__VA_ARGS__)
+#define _RREP(i,n) RREPI(i,n,0)
+#define RREPI(i,a,b) for(int i=int(a);i>=int(b);--i)
+#define RREP(...) _ovedtoad3(__VA_ARGS__,RREPI,_RREP,)(__VA_ARGS__)
+#define ALL(a) (a).begin(),(a).end()
+#define ALLR(a) (a).rbegin(),(a).rend()
+typedef long long ll;
+const int INF32 = 1001001001;
+const long long INF64 = 1001001001001001001;
+struct Init { Init() { ios::sync_with_stdio(0); cin.tie(0); cout << setprecision(15); }} init;
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> T gcd(T x, T y){ return (x % y) ? gcd(y, x % y) : y; }
+template<class T> T lcm(T x, T y){ return x / gcd(x, y) * y; }
+template<class T, class... Ts> void output(const T& a, const Ts&... b) { cout << a; (cout << ... << (cout << ' ', b)); cout << '\n'; }
+template<class T> void output(vector<T> v) { for (auto u : v) cout << u << ' '; cout << '\n'; };
+void yesno(bool is_ok) { cout << (is_ok ? "yes" : "no") << '\n'; }
+void YesNo(bool is_ok) { cout << (is_ok ? "Yes" : "No") << '\n'; }
+void YESNO(bool is_ok) { cout << (is_ok ? "YES" : "NO") << '\n'; }
+
+// clang-format on
+int main() {
+    int n;
+    cin >> n;
+    vector<pair<int, int>> dt(n);
+    REP(i, n) cin >> dt[i].second >> dt[i].first;
+    sort(ALL(dt));
+    vector dp(n + 1, vector<int>(1441, 0));
+    REP(i, n) {
+        auto [d, t] = dt[i];
+        REP(j, 1441) {
+            chmax(dp[i + 1][j], dp[i][j]);
+            if (j - 1 >= 0) chmax(dp[i + 1][j], dp[i + 1][j - 1]);
+            if (j - t >= 0 && j <= d) {
+                chmax(dp[i + 1][j], dp[i][j - t] + 1);
+            }
+        }
+    }
+    int ans = 0;
+    REP(i, 1441) chmax(ans, dp[n][i]);
+    output(ans);
+}
---
+++ tessoku/10/a77.cpp
@@ -0,0 +1,63 @@
+// clang-format off
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+#include <atcoder/all>
+using namespace atcoder;
+#endif
+
+#define _overload3(_1,_2,_3,name,...) name
+#define _REP(i,n) REPI(i,0,n)
+#define REPI(i,a,b) for(int i=int(a);i<int(b);++i)
+#define REP(...) _overload3(__VA_ARGS__,REPI,_REP,)(__VA_ARGS__)
+#define _RREP(i,n) RREPI(i,n,0)
+#define RREPI(i,a,b) for(int i=int(a);i>=int(b);--i)
+#define RREP(...) _overload3(__VA_ARGS__,RREPI,_RREP,)(__VA_ARGS__)
+#define ALL(a) (a).begin(),(a).end()
+#define ALLR(a) (a).rbegin(),(a).rend()
+typedef long long ll;
+const int INF32 = 1001001001;
+const long long INF64 = 1001001001001001001;
+struct Init { Init() { ios::sync_with_stdio(0); cin.tie(0); cout << setprecision(15); }} init;
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> T gcd(T x, T y){ return (x % y) ? gcd(y, x % y) : y; }
+template<class T> T lcm(T x, T y){ return x / gcd(x, y) * y; }
+template<class T, class... Ts> void output(const T& a, const Ts&... b) { cout << a; (cout << ... << (cout << ' ', b)); cout << '\n'; }
+template<class T> void output(vector<T> v) { for (auto u : v) cout << u << ' '; cout << '\n'; };
+void yesno(bool is_ok) { cout << (is_ok ? "yes" : "no") << '\n'; }
+void YesNo(bool is_ok) { cout << (is_ok ? "Yes" : "No") << '\n'; }
+void YESNO(bool is_ok) { cout << (is_ok ? "YES" : "NO") << '\n'; }
+
+// clang-format on
+int main() {
+    int n, l, k;
+    cin >> n >> l >> k;
+    vector<int> a(n + 2, 0);
+    REP(i, n) cin >> a[i + 1];
+    a[n + 1] = l;
+    int ok = 0;
+    int ng = INF32;
+    while (ng - ok != 1) {
+        int mid = (ok + ng) / 2;
+        int pos = 0;
+        int cnt = 0;
+        bool is_ok = false;
+        REP(i, n + 2) {
+            if (a[i] - a[pos] >= mid) {
+                pos = i;
+                ++cnt;
+            }
+            if (cnt == k + 1) {
+                is_ok = true;
+                break;
+            }
+        }
+        if (is_ok) {
+            ok = mid;
+        } else {
+            ng = mid;
+        }
+    }
+    output(ok);
+}
