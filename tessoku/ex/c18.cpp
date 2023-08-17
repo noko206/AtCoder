c87f2977c089979f428c9d335835a1f839be99bb
noko206 <61347127+noko206@users.noreply.github.com>
2023-08-17 20:57:07 +0900

    add: 鉄則 力試し問題 途中

---
+++ tessoku/ex/c18.cpp
@@ -0,0 +1,54 @@
+// clang-format off
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+#include <atcoder/all>
+using namespace atcoder;
+#endif
+
+#define _overload3(_1,_2,_3,name,...) name
+#define _REP(i,n) REPI(i,0,n)
+#define REPI(i,a,b) for(int i=int(a);i<int(b);++i)
+#define REP(...) _overload3(__VA_ARGS__,REPI,_REP,)(__VA_ARGS__)
+#define _RREP(i,n) RREPI(i,n,0)
+#define RREPI(i,a,b) for(int i=int(a);i>=int(b);--i)
+#define RREP(...) _overload3(__VA_ARGS__,RREPI,_RREP,)(__VA_ARGS__)
+#define ALL(a) (a).begin(),(a).end()
+#define ALLR(a) (a).rbegin(),(a).rend()
+typedef long long ll;
+const int INF32 = 1001001001;
+const long long INF64 = 1001001001001001001;
+struct Init { Init() { ios::sync_with_stdio(0); cin.tie(0); cout << setprecision(15); }} init;
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> T gcd(T x, T y){ return (x % y) ? gcd(y, x % y) : y; }
+template<class T> T lcm(T x, T y){ return x / gcd(x, y) * y; }
+template<class T, class... Ts> void output(const T& a, const Ts&... b) { cout << a; (cout << ... << (cout << ' ', b)); cout << '\n'; }
+template<class T> void output(vector<T> v) { for (auto u : v) cout << u << ' '; cout << '\n'; };
+void yesno(bool is_ok) { cout << (is_ok ? "yes" : "no") << '\n'; }
+void YesNo(bool is_ok) { cout << (is_ok ? "Yes" : "No") << '\n'; }
+void YESNO(bool is_ok) { cout << (is_ok ? "YES" : "NO") << '\n'; }
+
+// clang-format on
+int main() {
+    int n;
+    cin >> n;
+    int m = n * 2;
+    vector<int> a(m);
+    REP(i, m) cin >> a[i];
+    vector dp(m, vector<int>(m, INF32));
+    auto dfs = [&](auto &dfs, int l, int r) -> int {
+        assert((r - l + 1) % 2 == 0);
+        if (r <= l) return 0;
+        if (l + 1 == r) return dp[l][r] = abs(a[l] - a[r]);
+        if (dp[l][r] != INF32) return dp[l][r];
+        int ans = INF32;
+        for (int i = l + 1; i <= r - 2; i += 2) {
+            chmin(ans, dfs(dfs, l, i) + dfs(dfs, i + 1, r));
+        }
+        chmin(ans, abs(a[l] - a[r]) + dfs(dfs, l + 1, r - 1));
+        return dp[l][r] = ans;
+    };
+    dfs(dfs, 0, m - 1);
+    output(dp[0][m - 1]);
+}
