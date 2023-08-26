58dd253cb27e1d1a7590302dfc2e63d30b2d5f62
noko206 <61347127+noko206@users.noreply.github.com>
2023-08-26 12:03:54 +0900

    add: 精進

---
+++ edpc/n.cpp
@@ -0,0 +1,85 @@
+// clang-format off
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+#include <atcoder/all>
+using namespace atcoder;
+#endif
+
+#define _overload3(_1,_2,_3,name,...) name
+#define _REP(i,n) REPI(i,0,n)
+#define REPI(i,a,b) for(int i=int(a);i<int(b);++i)
+#define REP(...) _overload3(__VA_ARGS__,REPI,_REP,)(__VA_ARGS__)
+#define _RREP(i,n) RREPI(i,n,0)
+#define RREPI(i,a,b) for(int i=int(a);i>=int(b);--i)
+#define RREP(...) _overload3(__VA_ARGS__,RREPI,_RREP,)(__VA_ARGS__)
+#define ALL(a) (a).begin(),(a).end()
+#define ALLR(a) (a).rbegin(),(a).rend()
+typedef long long ll;
+const int INF32 = 1001001001;
+const long long INF64 = 1001001001001001001;
+struct Init { Init() { ios::sync_with_stdio(0); cin.tie(0); cout << setprecision(15); }} init;
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> T gcd(T x, T y){ return (x % y) ? gcd(y, x % y) : y; }
+template<class T> T lcm(T x, T y){ return x / gcd(x, y) * y; }
+template<class T, class... Ts> void output(const T& a, const Ts&... b) { cout << a; (cout << ... << (cout << ' ', b)); cout << '\n'; }
+template<class T> void output(vector<T> v) { for (auto u : v) cout << u << ' '; cout << '\n'; };
+void yesno(bool is_ok) { cout << (is_ok ? "yes" : "no") << '\n'; }
+void YesNo(bool is_ok) { cout << (is_ok ? "Yes" : "No") << '\n'; }
+void YESNO(bool is_ok) { cout << (is_ok ? "YES" : "NO") << '\n'; }
+
+// clang-format on
+template <class T> struct CumulativeSum {
+   public:
+    CumulativeSum(int n) : n(n), data(n + 1, 0){};
+    CumulativeSum(const vector<T> &v) : n((int)v.size()) {
+        data.resize(n + 1, 0);
+        for (int i = 0; i < n; ++i) {
+            data[i + 1] = v[i];
+        }
+    };
+
+    void add(int idx, T x) {
+        assert(0 <= idx && idx < n);
+        data[idx + 1] += x;
+    }
+
+    void build() {
+        for (int i = 0; i < n; ++i) {
+            data[i + 1] += data[i];
+        }
+    }
+
+    // [l, r)
+    T sum(int l, int r) {
+        assert(0 <= l && l <= r && r <= n);
+        return data[r] - data[l];
+    }
+
+   private:
+    int n;
+    vector<T> data;
+};
+
+int main() {
+    int n;
+    cin >> n;
+    vector<ll> a(n);
+    REP(i, n) cin >> a[i];
+    CumulativeSum<ll> cs(a);
+    cs.build();
+    vector dp(n, vector<ll>(n, INF64));
+    auto dfs = [&](auto &dfs, int l, int r) -> ll {
+        if (l == r) return 0;
+        if (dp[l][r] != INF64) return dp[l][r];
+        ll ans = INF64;
+        REP(i, l, r) {
+            ll tmp = dfs(dfs, l, i) + dfs(dfs, i + 1, r);
+            tmp += cs.sum(l, i + 1) + cs.sum(i + 1, r + 1);
+            chmin(ans, tmp);
+        }
+        return dp[l][r] = ans;
+    };
+    output(dfs(dfs, 0, n - 1));
+}
