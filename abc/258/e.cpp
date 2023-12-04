25e7da9f80df3d913157dba93956ed2416d64261
noko206 <61347127+noko206@users.noreply.github.com>
2023-12-05 00:52:37 +0900

    add: 精進

--- abc/258/e.cpp
+++ abc/258/e.cpp
@@ -1,48 +1,117 @@
+// clang-format off
 #include <bits/stdc++.h>
 using namespace std;
+#if __has_include(<atcoder/all>)
+#include <atcoder/all>
+using namespace atcoder;
+#endif
 
 #define _overload3(_1,_2,_3,name,...) name
 #define _REP(i,n) REPI(i,0,n)
 #define REPI(i,a,b) for(int i=int(a);i<int(b);++i)
 #define REP(...) _overload3(__VA_ARGS__,REPI,_REP,)(__VA_ARGS__)
+#define _RREP(i,n) RREPI(i,n,0)
+#define RREPI(i,a,b) for(int i=int(a);i>=int(b);--i)
+#define RREP(...) _overload3(__VA_ARGS__,RREPI,_RREP,)(__VA_ARGS__)
 #define ALL(a) (a).begin(),(a).end()
 #define ALLR(a) (a).rbegin(),(a).rend()
 typedef long long ll;
-typedef long double ld;
-const int MOD = 1000000007;
 const int INF32 = 1001001001;
 const long long INF64 = 1001001001001001001;
-template<class T> bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
-template<class T> bool chmin(T &a, const T &b) { if (a>b) { a=b; return 1; } return 0; }
-ll gcd(ll x, ll y){ return (x % y) ? gcd(y, x % y) : y; }
-ll lcm(ll x, ll y){ return x / gcd(x, y) * y; }
+struct Init { Init() { ios::sync_with_stdio(0); cin.tie(0); cout << setprecision(15); }} init;
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> T gcd(T x, T y){ return (x % y) ? gcd(y, x % y) : y; }
+template<class T> T lcm(T x, T y){ return x / gcd(x, y) * y; }
+template<class T, class... Ts> void output(const T& a, const Ts&... b) { cout << a; (cout << ... << (cout << ' ', b)); cout << '\n'; }
+template<class T> void output(vector<T> v) { for (auto u : v) cout << u << ' '; cout << '\n'; };
+void yesno(bool is_ok) { cout << (is_ok ? "yes" : "no") << '\n'; }
+void YesNo(bool is_ok) { cout << (is_ok ? "Yes" : "No") << '\n'; }
+void YESNO(bool is_ok) { cout << (is_ok ? "YES" : "NO") << '\n'; }
 
-int main(){
-	int n, q, x;
-	cin >> n >> q >> x;
-	vector<int> w(n);
-	REP(i, n){
-		cin >> w[i];
-	}
-	map<int, ll> mp;
-	int idx = 0;
-	while(mp.find(idx) == mp.end()){
-		int now = idx;
-		ll tot = 0;
-		while(tot < (ll)x){
-			tot += w[now];
-			now = (now + 1) % n;
-		}
-		mp[idx] = tot;
-		idx = now;
-	}
-	if(idx != 0){
-		
-	}
-	vector<ll> ans(q);
-	REP(i, q){
-		ll k;
-		cin >> k;
+// clang-format on
+struct Doubling {
+   public:
+    Doubling(int n) : n(n), dp(61, vector<int>(n)) {
+        for (int i = 0; i < n; ++i) {
+            dp[0][i] = i;
+        }
+    };
 
-	}
+    Doubling(const vector<int> &a)
+        : n(a.size()), dp(61, vector<int>(a.size())) {
+        dp[0] = a;
+    };
+
+    // 0-indexed
+    void add_edge(int v, int u) { dp[0][v] = u; }
+
+    void build() {
+        for (int i = 0; i < 60; ++i) {
+            for (int j = 0; j < n; ++j) {
+                dp[i + 1][j] = dp[i][dp[i][j]];
+            }
+        }
+    }
+
+    // 0-indexed
+    int query(int x, ll y) {
+        int ans = x;
+        for (int i = 0; i < 61; ++i) {
+            if (y & (1LL << i)) {
+                ans = dp[i][ans];
+            }
+        }
+        return ans;
+    }
+
+   private:
+    int n;
+    vector<vector<int>> dp;
+};
+
+int main() {
+    int n, q, x;
+    cin >> n >> q >> x;
+    vector<int> w(n);
+    REP(i, n) cin >> w[i];
+    fenwick_tree<ll> ft(n * 2);
+    REP(i, n * 2) ft.add(i, w[i % n]);
+    ll sum = ft.sum(0, n);
+    vector<ll> cnt(n);
+    auto bs = [&](int ok, int ng, int x, int i) -> int {
+        while (ok - ng != 1) {
+            int mid = (ok + ng) / 2;
+            if (ft.sum(i, mid + 1) >= x) {
+                ok = mid;
+            } else {
+                ng = mid;
+            }
+        }
+        return ok;
+    };
+    REP(i, n) {
+        int ok = bs(n * 2, i - 1, x, i);
+        if (ok == n * 2) {
+            ll c = n - i;
+            ll nokori = x - ft.sum(i, n);
+            c += (nokori / sum) * n;
+            nokori %= sum;
+            c += bs(n, -1, nokori, 0) + 1;
+            cnt[i] = c;
+        } else {
+            cnt[i] = ok - i + 1;
+        }
+    }
+    vector<int> a(n);
+    REP(i, n) a[i] = (i + cnt[i]) % n;
+    Doubling d(a);
+    d.build();
+    vector<ll> ans(q);
+    REP(i, q) {
+        ll k;
+        cin >> k;
+        ans[i] = cnt[d.query(0, k - 1)];
+    }
+    REP(i, q) output(ans[i]);
 }
--- abc/265/e.cpp
+++ abc/265/e.cpp
@@ -1,82 +1,77 @@
+// clang-format off
 #include <bits/stdc++.h>
 using namespace std;
+#if __has_include(<atcoder/all>)
+#include <atcoder/all>
+using namespace atcoder;
+#endif
 
 #define _overload3(_1,_2,_3,name,...) name
 #define _REP(i,n) REPI(i,0,n)
 #define REPI(i,a,b) for(int i=int(a);i<int(b);++i)
 #define REP(...) _overload3(__VA_ARGS__,REPI,_REP,)(__VA_ARGS__)
+#define _RREP(i,n) RREPI(i,n,0)
+#define RREPI(i,a,b) for(int i=int(a);i>=int(b);--i)
+#define RREP(...) _overload3(__VA_ARGS__,RREPI,_RREP,)(__VA_ARGS__)
 #define ALL(a) (a).begin(),(a).end()
 #define ALLR(a) (a).rbegin(),(a).rend()
 typedef long long ll;
-const int MOD1000000007 = 1000000007;
-const int MOD998244353 = 998244353;
 const int INF32 = 1001001001;
 const long long INF64 = 1001001001001001001;
-template<class T> bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
-template<class T> bool chmin(T &a, const T &b) { if (a>b) { a=b; return 1; } return 0; }
-ll gcd(ll x, ll y){ return (x % y) ? gcd(y, x % y) : y; }
-ll lcm(ll x, ll y){ return x / gcd(x, y) * y; }
+struct Init { Init() { ios::sync_with_stdio(0); cin.tie(0); cout << setprecision(15); }} init;
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> T gcd(T x, T y){ return (x % y) ? gcd(y, x % y) : y; }
+template<class T> T lcm(T x, T y){ return x / gcd(x, y) * y; }
+template<class T, class... Ts> void output(const T& a, const Ts&... b) { cout << a; (cout << ... << (cout << ' ', b)); cout << '\n'; }
+template<class T> void output(vector<T> v) { for (auto u : v) cout << u << ' '; cout << '\n'; };
+void yesno(bool is_ok) { cout << (is_ok ? "yes" : "no") << '\n'; }
+void YesNo(bool is_ok) { cout << (is_ok ? "Yes" : "No") << '\n'; }
+void YESNO(bool is_ok) { cout << (is_ok ? "YES" : "NO") << '\n'; }
 
-ll mod_pow(ll a, ll n, ll p) {
-	if (n == 0) return 1;
-	if (n == 1) return a % p;
-	if (n % 2 == 1) return (a * mod_pow(a, n - 1, p)) % p;
-	ll t = mod_pow(a, n / 2, p);
-	return (t * t) % p;
-}
+// clang-format on
+using mint = modint998244353;
 
-ll mod_inv(ll a, ll m) {
-	ll b = m, u = 1, v = 0;
-	while (b) {
-		ll t = a / b;
-		a -= t * b; swap(a, b);
-		u -= t * v; swap(u, v);
-	}
-	u %= m;
-	if (u < 0) u += m;
-	return u;
-}
+mint dp[305][305][305];
 
-int main(){
-	int n, m;
-	cin >> n >> m;
-	ll a, b, c, d, e, f;
-	cin >> a >> b >> c >> d >> e >> f;
-	map<pair<ll, ll>, bool> mp;
-	REP(i, m){
-		int x, y;
-		cin >> x >> y;
-		mp[{x, y}] = true;
-	}
-	int mod = MOD998244353;
-	ll ans = mod_pow(3, n, mod);
-	vector<ll> fact(n + 1); fact[0] = 1;
-	vector<ll> ifact(n + 1); ifact[0] = mod_inv(1, mod);
-	REP(i, n){
-		fact[i + 1] = (i + 1) * fact[i];
-		fact[i + 1] %= mod;
-		ifact[i + 1] = mod_inv(fact[i + 1], mod);
-	}
-	REP(i, n + 1){
-		REP(j, n + 1){
-			REP(k, n + 1){
-				if(i + j + k > n) continue;
-				ll x = a * i + c * j + e * k;
-				ll y = b * i + d * j + f * k;
-				if(mp.find({x, y}) != mp.end()){
-					ll tmp = fact[i + j + k];
-					tmp *= ifact[i]; tmp %= mod;
-					tmp *= ifact[j]; tmp %= mod;
-					tmp *= ifact[k]; tmp %= mod;
-					tmp *= mod_pow(3, n - (i + j + k), mod);
-					tmp %= mod;
-					ans -= tmp;
-					ans += mod;
-					ans %= mod;
-					break;
-				}
-			}
-		}
-	}
-	cout << ans << endl;
+int main() {
+    int n, m;
+    cin >> n >> m;
+    ll a, b, c, d, e, f;
+    cin >> a >> b >> c >> d >> e >> f;
+    map<pair<ll, ll>, bool> mp;
+    REP(i, m) {
+        int x, y;
+        cin >> x >> y;
+        mp[{x, y}] = true;
+    }
+    dp[0][0][0] = 1;
+    REP(i, n) REP(j, i + 1) REP(k, i + 1 - j) {
+        {
+            ll x = j * a + k * c + (i + 1 - j - k) * e;
+            ll y = j * b + k * d + (i + 1 - j - k) * f;
+            if (mp.find({x, y}) == mp.end()) {
+                dp[i + 1][j][k] += dp[i][j][k];
+            }
+        }
+        {
+            ll x = (j + 1) * a + k * c + (i - j - k) * e;
+            ll y = (j + 1) * b + k * d + (i - j - k) * f;
+            if (j + 1 <= n && mp.find({x, y}) == mp.end()) {
+                dp[i + 1][j + 1][k] += dp[i][j][k];
+            }
+        }
+        {
+            ll x = j * a + (k + 1) * c + (i - j - k) * e;
+            ll y = j * b + (k + 1) * d + (i - j - k) * f;
+            if (k + 1 <= n && mp.find({x, y}) == mp.end()) {
+                dp[i + 1][j][k + 1] += dp[i][j][k];
+            }
+        }
+    }
+    mint ans = 0;
+    REP(i, n + 1) {
+        REP(j, n + 1) { ans += dp[n][i][j]; }
+    }
+    output(ans.val());
 }
