c02b6785a0e34e89264a1ceacfc1ba324ef6ce4c
noko206 <61347127+noko206@users.noreply.github.com>
2023-12-18 23:17:00 +0900

    add: 精進

---
+++ abc/191/e.cpp
@@ -0,0 +1,74 @@
+// clang-format off
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+#include <atcoder/all>
+using namespace atcoder;
+#endif
+
+#define _overload3(_1,_2,_3,name,...) name
+#define _REP(i,n) REPI(i,0,n)
+#define REPI(i,a,b) for(int i=int(a);i<int(b);++i)
+#define REP(...) _overload3(__VA_ARGS__,REPI,_REP,)(__VA_ARGS__)
+#define _RREP(i,n) RREPI(i,n,0)
+#define RREPI(i,a,b) for(int i=int(a);i>=int(b);--i)
+#define RREP(...) _overload3(__VA_ARGS__,RREPI,_RREP,)(__VA_ARGS__)
+#define ALL(a) (a).begin(),(a).end()
+#define ALLR(a) (a).rbegin(),(a).rend()
+typedef long long ll;
+const int INF32 = 1001001001;
+const long long INF64 = 1001001001001001001;
+struct Init { Init() { ios::sync_with_stdio(0); cin.tie(0); cout << setprecision(15); }} init;
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> T gcd(T x, T y){ return (x % y) ? gcd(y, x % y) : y; }
+template<class T> T lcm(T x, T y){ return x / gcd(x, y) * y; }
+template<class T, class... Ts> void output(const T& a, const Ts&... b) { cout << a; (cout << ... << (cout << ' ', b)); cout << '\n'; }
+template<class T> void output(vector<T> v) { for (auto u : v) cout << u << ' '; cout << '\n'; };
+void yesno(bool is_ok) { cout << (is_ok ? "yes" : "no") << '\n'; }
+void YesNo(bool is_ok) { cout << (is_ok ? "Yes" : "No") << '\n'; }
+void YESNO(bool is_ok) { cout << (is_ok ? "YES" : "NO") << '\n'; }
+
+// clang-format on
+using P = pair<ll, int>;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<vector<P>> to(n);
+    REP(i, m) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        --a, --b;
+        to[a].emplace_back(c, b);
+    }
+    vector<ll> ans(n);
+    REP(s, n) {
+        priority_queue<P, vector<P>, greater<P>> pq;
+        pq.push({0, s});
+        vector<ll> dist(n, INF64);
+        dist[s] = 0;
+        bool is_first = true;
+        while (!pq.empty()) {
+            auto [vc, v] = pq.top();
+            pq.pop();
+            if (dist[v] < vc) continue;
+            for (auto [uc, u] : to[v]) {
+                if (chmin(dist[u], dist[v] + uc)) {
+                    pq.push({dist[u], u});
+                }
+            }
+            if (is_first) {
+                dist[s] = INF64;
+                is_first = false;
+            }
+        }
+        for (auto [vc, v] : to[s]) {
+            if (v == s) {
+                chmin(dist[s], vc);
+            }
+        }
+        ans[s] = dist[s] == INF64 ? -1 : dist[s];
+    }
+    REP(i, n) output(ans[i]);
+}
