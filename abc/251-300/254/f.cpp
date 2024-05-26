9ba65e4d4391be66a4b23c3671ea86ce3b3a014e
noko206 <61347127+noko206@users.noreply.github.com>
2024-05-26 21:10:19 +0900

    精進

--- abc/251-300/254/f.cpp
+++ abc/251-300/254/f.cpp
@@ -21,7 +21,7 @@ const long long INF64 = 1001001001001001001;
 struct Init { Init() { ios::sync_with_stdio(0); cin.tie(0); cout << setprecision(15); }} init;
 template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
 template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
-template<class T> T gcd(T x, T y){ return (x % y) ? gcd(y, x % y) : y; }
+template<class T> T gcd(T x, T y){ if(x == 0) return y; if(y == 0) return x; return (x % y) ? gcd(y, x % y) : y; }
 template<class T> T lcm(T x, T y){ return x / gcd(x, y) * y; }
 template<class T, class... Ts> void output(const T& a, const Ts&... b) { cout << a; (cout << ... << (cout << ' ', b)); cout << '\n'; }
 template<class T> void output(vector<T> v) { for (auto u : v) cout << u << ' '; cout << '\n'; };
@@ -30,27 +30,27 @@ void YesNo(bool is_ok) { cout << (is_ok ? "Yes" : "No") << '\n'; }
 void YESNO(bool is_ok) { cout << (is_ok ? "YES" : "NO") << '\n'; }
 
 // clang-format on
-int op(int a, int b) {
-    assert(a != -1 || b != -1);
-    if (a == -1) return b;
-    if (b == -1) return a;
-    return gcd(a, b);
-}
+int op(int a, int b) { return gcd(a, b); }
 
-int e() { return -1; }
+int e() { return 0; }
 
 int main() {
     int n, q;
     cin >> n >> q;
     vector<int> a(n), b(n);
-    REP(i, n) cin >> a[i] >> b[i];
-    segtree<int, op, e> segH(a), segW(b);
+    REP(i, n) cin >> a[i];
+    REP(i, n) cin >> b[i];
+    segtree<int, op, e> segH(n), segW(n);
+    REP(i, 1, n) {
+        segH.set(i, abs(a[i] - a[i - 1]));
+        segW.set(i, abs(b[i] - b[i - 1]));
+    }
     vector<int> ans(q);
     REP(i, q) {
-        int hs, hg, ws, wg;
-        cin >> hs >> hg >> ws >> wg;
-        --hs, --ws;
-        ans[i] = gcd(segH.prod(hs, hg), segW.prod(ws, wg));
+        int h1, h2, w1, w2;
+        cin >> h1 >> h2 >> w1 >> w2;
+        int g = gcd(segH.prod(h1, h2), segW.prod(w1, w2));
+        ans[i] = gcd(a[h1 - 1] + b[w1 - 1], g);
     }
-    REP(i, q) output(ans[i]);
+    REP(i, ans.size()) output(ans[i]);
 }
