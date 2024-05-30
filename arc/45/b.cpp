4435146805ba20172f6b894c16d56c2028a0767e
noko206 <61347127+noko206@users.noreply.github.com>
2024-05-30 22:17:00 +0900

    精進

---
+++ arc/45/b.cpp
@@ -0,0 +1,60 @@
+// clang-format off
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+#include <atcoder/all>
+using namespace atcoder;
+#endif
+
+#define _overload3(_1,_2,_3,name,...) name
+#define _REP(i,n) REPI(i,0,n)
+#define REPI(i,a,b) for(int i=int(a);i<int(b);++i)
+#define REP(...) _overload3(__VA_ARGS__,REPI,_REP,)(__VA_ARGS__)
+#define _RREP(i,n) RREPI(i,n,0)
+#define RREPI(i,a,b) for(int i=int(a);i>=int(b);--i)
+#define RREP(...) _overload3(__VA_ARGS__,RREPI,_RREP,)(__VA_ARGS__)
+#define ALL(a) (a).begin(),(a).end()
+#define ALLR(a) (a).rbegin(),(a).rend()
+typedef long long ll;
+const int INF32 = 1001001001;
+const long long INF64 = 1001001001001001001;
+struct Init { Init() { ios::sync_with_stdio(0); cin.tie(0); cout << setprecision(15); }} init;
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> T gcd(T x, T y){ return (x % y) ? gcd(y, x % y) : y; }
+template<class T> T lcm(T x, T y){ return x / gcd(x, y) * y; }
+template<class T, class... Ts> void output(const T& a, const Ts&... b) { cout << a; (cout << ... << (cout << ' ', b)); cout << '\n'; }
+template<class T> void output(vector<T> v) { for (auto u : v) cout << u << ' '; cout << '\n'; };
+void yesno(bool is_ok) { cout << (is_ok ? "yes" : "no") << '\n'; }
+void YesNo(bool is_ok) { cout << (is_ok ? "Yes" : "No") << '\n'; }
+void YESNO(bool is_ok) { cout << (is_ok ? "YES" : "NO") << '\n'; }
+
+// clang-format on
+using S = long long;
+using F = long long;
+
+S op(S a, S b) { return std::min(a, b); }
+S e() { return INF64; }
+S mapping(F f, S x) { return f + x; }
+F composition(F f, F g) { return f + g; }
+F id() { return 0; }
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<int> s(n), t(n);
+    REP(i, m) cin >> s[i] >> t[i], --s[i];
+    lazy_segtree<S, op, e, F, mapping, composition, id> seg(n);
+    REP(i, n) seg.set(i, 0);
+    REP(i, m) { seg.apply(s[i], t[i], 1); }
+    vector<int> ans;
+    REP(i, m) {
+        seg.apply(s[i], t[i], -1);
+        if (seg.all_prod() > 0) {
+            ans.push_back(i + 1);
+        }
+        seg.apply(s[i], t[i], 1);
+    }
+    output(ans.size());
+    REP(i, ans.size()) output(ans[i]);
+}
