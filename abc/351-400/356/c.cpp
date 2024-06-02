350b965cd1c0b6e3cc6603197ccef8d1d60f1922
noko206 <61347127+noko206@users.noreply.github.com>
2024-06-03 00:42:35 +0900

    ABC

---
+++ abc/351-400/356/c.cpp
@@ -0,0 +1,61 @@
+// clang-format off
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+#include <atcoder/all>
+using namespace atcoder;
+#endif
+
+#define _overload3(_1,_2,_3,name,...) name
+#define _REP(i,n) REPI(i,0,n)
+#define REPI(i,a,b) for(int i=int(a);i<int(b);++i)
+#define REP(...) _overload3(__VA_ARGS__,REPI,_REP,)(__VA_ARGS__)
+#define _RREP(i,n) RREPI(i,n,0)
+#define RREPI(i,a,b) for(int i=int(a);i>=int(b);--i)
+#define RREP(...) _overload3(__VA_ARGS__,RREPI,_RREP,)(__VA_ARGS__)
+#define ALL(a) (a).begin(),(a).end()
+#define ALLR(a) (a).rbegin(),(a).rend()
+typedef long long ll;
+const int INF32 = 1001001001;
+const long long INF64 = 1001001001001001001;
+struct Init { Init() { ios::sync_with_stdio(0); cin.tie(0); cout << setprecision(15); }} init;
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> T gcd(T x, T y){ return (x % y) ? gcd(y, x % y) : y; }
+template<class T> T lcm(T x, T y){ return x / gcd(x, y) * y; }
+template<class T, class... Ts> void output(const T& a, const Ts&... b) { cout << a; (cout << ... << (cout << ' ', b)); cout << '\n'; }
+template<class T> void output(vector<T> v) { for (auto u : v) cout << u << ' '; cout << '\n'; };
+void yesno(bool is_ok) { cout << (is_ok ? "yes" : "no") << '\n'; }
+void YesNo(bool is_ok) { cout << (is_ok ? "Yes" : "No") << '\n'; }
+void YESNO(bool is_ok) { cout << (is_ok ? "YES" : "NO") << '\n'; }
+
+// clang-format on
+int main() {
+    int n, m, k;
+    cin >> n >> m >> k;
+    vector<int> c(m);
+    vector<vector<int>> a(m);
+    vector<char> r(m);
+    REP(i, m) {
+        cin >> c[i];
+        a[i].resize(c[i]);
+        REP(j, c[i]) cin >> a[i][j], --a[i][j];
+        cin >> r[i];
+    }
+    int ans = 0;
+    REP(bit, 1 << n) {
+        bool is_ok = true;
+        REP(i, m) {
+            int cnt = 0;
+            REP(j, c[i]) {
+                if (bit & (1 << a[i][j])) {
+                    ++cnt;
+                }
+            }
+            if (cnt < k && r[i] == 'o') is_ok = false;
+            if (cnt >= k && r[i] == 'x') is_ok = false;
+        }
+        if (is_ok) ++ans;
+    }
+    output(ans);
+}
