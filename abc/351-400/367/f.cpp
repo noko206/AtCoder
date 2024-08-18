1db033c86726f9bccac71bee404d6410db34190a
noko206 <61347127+noko206@users.noreply.github.com>
2024-08-18 05:08:01 +0000

    ABC367

---
+++ abc/351-400/367/f.cpp
@@ -0,0 +1,90 @@
+// clang-format off
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+#include <atcoder/all>
+using namespace atcoder;
+#endif
+
+#define _overload3(_1,_2,_3,name,...) name
+#define _REP(i,n) REPI(i,0,n)
+#define REPI(i,a,b) for(int i=int(a);i<int(b);++i)
+#define REP(...) _overload3(__VA_ARGS__,REPI,_REP,)(__VA_ARGS__)
+#define _RREP(i,n) RREPI(i,n,0)
+#define RREPI(i,a,b) for(int i=int(a);i>=int(b);--i)
+#define RREP(...) _overload3(__VA_ARGS__,RREPI,_RREP,)(__VA_ARGS__)
+#define ALL(a) (a).begin(),(a).end()
+#define ALLR(a) (a).rbegin(),(a).rend()
+typedef long long ll;
+const int INF32 = 1001001001;
+const long long INF64 = 1001001001001001001;
+struct Init { Init() { ios::sync_with_stdio(0); cin.tie(0); cout << setprecision(15); }} init;
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> T gcd(T x, T y){ return (x % y) ? gcd(y, x % y) : y; }
+template<class T> T lcm(T x, T y){ return x / gcd(x, y) * y; }
+template<class T, class... Ts> void output(const T& a, const Ts&... b) { cout << a; (cout << ... << (cout << ' ', b)); cout << '\n'; }
+template<class T> void output(vector<T> v) { for (auto u : v) cout << u << ' '; cout << '\n'; };
+void yesno(bool is_ok) { cout << (is_ok ? "yes" : "no") << '\n'; }
+void YesNo(bool is_ok) { cout << (is_ok ? "Yes" : "No") << '\n'; }
+void YESNO(bool is_ok) { cout << (is_ok ? "YES" : "NO") << '\n'; }
+
+// clang-format on
+struct Mo {
+   public:
+    Mo(int n) : n(n) {};
+
+    void add(int l, int r) { lr.emplace_back(l, r); }
+
+    void build(const F &add_left, const F &add_right, const F &erase_left,
+               const F &erase_right, F out) {
+        int q = (int)lr.size();
+        int bs = n / min<int>(n, sqrt(q));
+        vector<int> ord(q);
+        iota(begin(ord), end(ord), 0);
+        sort(begin(ord), end(ord), [&](int a, int b) {
+            int ablock = lr[a].first / bs, bblock = lr[b].first / bs;
+            if (ablock != bblock) return ablock < bblock;
+            return (ablock & 1) ? lr[a].second > lr[b].second
+                                : lr[a].second < lr[b].second;
+        });
+        int l = 0, r = 0;
+        for (auto idx : ord) {
+            while (l > lr[idx].first) add_left(--l);
+            while (r < lr[idx].second) add_right(r++);
+            while (l < lr[idx].first) erase_left(l++);
+            while (r > lr[idx].second) erase_right(--r);
+            out(idx);
+        }
+    }
+
+    void build(const F &add, const F &erase, const F &out) {
+        build(add, add, erase, erase, out);
+    }
+
+   private:
+    using F = function<void(int)>;
+    int n;
+    vector<pair<int, int>> lr;
+}
+
+
+int main() {
+    int n, q;
+    cin >> n >> q;
+    vector<int> a(n), b(n);
+    REP(i, n) cin >> a[i];
+    REP(i, n) cin >> b[i];
+    Mo ma(n), mb(n);
+    segtree();
+    REP(i, q) {
+        int x, y, z, w;
+        cin >> x >> y >> z >> w;
+        --y, --w;
+        ma.add(x, y);
+        mb.add(z, w);
+    }
+    auto addA = [&](int i) -> void {
+
+    };
+}
