67e454aa7e7a453c7394c7d51589a08ce7b2d462
noko206 <61347127+noko206@users.noreply.github.com>
2024-09-29 13:16:05 +0000

    精進

--- abc/351-400/369/f.cpp
+++ abc/351-400/369/f.cpp
@@ -30,16 +30,79 @@ void YesNo(bool is_ok) { cout << (is_ok ? "Yes" : "No") << '\n'; }
 void YESNO(bool is_ok) { cout << (is_ok ? "YES" : "NO") << '\n'; }
 
 // clang-format on
+// LIS復元, LISの復元, LIS 復元
+// LISの復元は配列の添え字を返す
+template <class T> vector<T> LIS(const vector<T> &a, bool is_strong) {
+    int n = (int)a.size();
+    long long inf64 = 1001001001001001001;
+    vector<long long> dp(n, inf64);
+    vector<int> pos;
+    for (int i = 0; i < n; ++i) {
+        auto it = is_strong
+                      // 狭義単調増加
+                      ? lower_bound(dp.begin(), dp.end(), a[i])
+                      // 広義単調増加
+                      : upper_bound(dp.begin(), dp.end(), a[i]);
+        pos.push_back(it - dp.begin());
+        *it = a[i];
+    }
+    int lis = lower_bound(dp.begin(), dp.end(), inf64) - dp.begin();
+    vector<int> subseq(lis);
+    int si = subseq.size() - 1;
+    int pi = pos.size() - 1;
+    while (0 <= si && 0 <= pi) {
+        if (pos[pi] == si) {
+            subseq[si] = pi;
+            --si;
+        }
+        --pi;
+    }
+    return subseq;
+}
+
 int main() {
     int h, w, n;
     cin >> h >> w >> n;
-    vector<vector<int>> HtoW(h);
+    vector<pair<int, int>> rc(n);
     REP(i, n) {
         int r, c;
         cin >> r >> c;
         --r, --c;
-        HtoW[r].push_back(c);
+        rc[i] = {r, c};
     }
-    HtoW[h - 1].push_back(w - 1);
-    REP(i, h) sort(ALL(HtoW[i]));
+    rc.emplace_back(0, 0);
+    rc.emplace_back(h - 1, w - 1);
+    sort(ALL(rc));
+    vector<int> a;
+    for (auto [r, c] : rc) {
+        a.push_back(c);
+    }
+    auto lis = LIS(a, false);
+    string ans = "";
+    int now = 1;
+    int y = 0, x = 0;
+    bool is_first = true;
+    for (auto [r, c] : rc) {
+        if (is_first) {
+            is_first = false;
+            continue;
+        }
+        if (c != a[lis[now]]) {
+            continue;
+        }
+        while (y != r) {
+            ans += 'D';
+            ++y;
+        }
+        while (x != c) {
+            ans += 'R';
+            ++x;
+        }
+        ++now;
+        if (now == lis.size()) {
+            break;
+        }
+    }
+    output(lis.size() - 2);
+    output(ans);
 }
