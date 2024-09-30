8568fbc0d2202dfe2b967cf967f653dc9a7c8451
noko206 <61347127+noko206@users.noreply.github.com>
2024-09-30 15:19:21 +0000

    ABC367F

--- abc/351-400/367/f.cpp
+++ abc/351-400/367/f.cpp
@@ -30,44 +30,41 @@ void YesNo(bool is_ok) { cout << (is_ok ? "Yes" : "No") << '\n'; }
 void YESNO(bool is_ok) { cout << (is_ok ? "YES" : "NO") << '\n'; }
 
 // clang-format on
-struct Mo {
+template <class T> struct Zobrist_Hash {
    public:
-    Mo(int n) : n(n) {};
-
-    void add(int l, int r) { lr.emplace_back(l, r); }
-
-    void build(const F &add_left, const F &add_right, const F &erase_left,
-               const F &erase_right, F out) {
-        int q = (int)lr.size();
-        int bs = n / min<int>(n, sqrt(q));
-        vector<int> ord(q);
-        iota(begin(ord), end(ord), 0);
-        sort(begin(ord), end(ord), [&](int a, int b) {
-            int ablock = lr[a].first / bs, bblock = lr[b].first / bs;
-            if (ablock != bblock) return ablock < bblock;
-            return (ablock & 1) ? lr[a].second > lr[b].second
-                                : lr[a].second < lr[b].second;
-        });
-        int l = 0, r = 0;
-        for (auto idx : ord) {
-            while (l > lr[idx].first) add_left(--l);
-            while (r < lr[idx].second) add_right(r++);
-            while (l < lr[idx].first) erase_left(l++);
-            while (r > lr[idx].second) erase_right(--r);
-            out(idx);
+    Zobrist_Hash(vector<T> a) : n(a.size()) {
+        // ハッシュ割り当て
+        static unordered_map<T, long long> mp;
+        for (int i = 0; i < n; ++i) {
+            if (mp.find(a[i]) == mp.end()) {
+                mp[a[i]] = get_rand_range(1, mod - 1);
+            }
+        }
+        // 累積和
+        tot.resize(n + 1);
+        tot[0] = 0;
+        for (int i = 0; i < n; ++i) {
+            tot[i + 1] = (tot[i] + mp[a[i]]) % mod;
         }
     }
 
-    void build(const F &add, const F &erase, const F &out) {
-        build(add, add, erase, erase, out);
+    // [l, r)
+    long long hash(int l, int r) {
+        assert(0 <= l && l <= r && r <= n);
+        return (tot[r] - tot[l] + mod) % mod;
     }
 
    private:
-    using F = function<void(int)>;
+    static const long long mod = (1LL << 61) - 1;
     int n;
-    vector<pair<int, int>> lr;
-}
+    vector<long long> tot;
 
+    long long get_rand_range(long long min_val, long long max_val) {
+        static mt19937_64 mt64(0);
+        uniform_int_distribution<uint64_t> get_rand_uni_int(min_val, max_val);
+        return get_rand_uni_int(mt64);
+    }
+};
 
 int main() {
     int n, q;
@@ -75,16 +72,13 @@ int main() {
     vector<int> a(n), b(n);
     REP(i, n) cin >> a[i];
     REP(i, n) cin >> b[i];
-    Mo ma(n), mb(n);
-    segtree();
+    Zobrist_Hash hashA(a), hashB(b);
+    vector<bool> ans(q);
     REP(i, q) {
-        int x, y, z, w;
-        cin >> x >> y >> z >> w;
-        --y, --w;
-        ma.add(x, y);
-        mb.add(z, w);
+        int l, r, L, R;
+        cin >> l >> r >> L >> R;
+        --l, --L;
+        ans[i] = hashA.hash(l, r) == hashB.hash(L, R);
     }
-    auto addA = [&](int i) -> void {
-
-    };
+    REP(i, q) { YesNo(ans[i]); }
 }
