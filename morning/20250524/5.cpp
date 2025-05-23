98a8349b8136b8690c0e3b69c76e5e6957ec41f6
noko206 <61347127+noko206@users.noreply.github.com>
2025-05-23 23:31:43 +0000

    ばちゃ

---
+++ morning/20250524/5.cpp
@@ -0,0 +1,99 @@
+// clang-format off
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+#include <atcoder/all>
+using namespace atcoder;
+#endif
+
+#define _overload3(_1,_2,_3,name,...) name
+#define _REP(i,n) REPI(i,0,n)
+#define REPI(i,a,b) for(int i=int(a);i<int(b);++i)
+#define REP(...) _overload3(__VA_ARGS__,REPI,_REP,)(__VA_ARGS__)
+#define _RREP(i,n) RREPI(i,n,0)
+#define RREPI(i,a,b) for(int i=int(a);i>=int(b);--i)
+#define RREP(...) _overload3(__VA_ARGS__,RREPI,_RREP,)(__VA_ARGS__)
+#define ALL(a) (a).begin(),(a).end()
+#define ALLR(a) (a).rbegin(),(a).rend()
+typedef long long ll;
+const int INF32 = 1001001001;
+const long long INF64 = 1001001001001001001;
+struct Init { Init() { ios::sync_with_stdio(0); cin.tie(0); cout << setprecision(15); }} init;
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> T gcd(T x, T y){ return (x % y) ? gcd(y, x % y) : y; }
+template<class T> T lcm(T x, T y){ return x / gcd(x, y) * y; }
+template<class T, class... Ts> void output(const T& a, const Ts&... b) { cout << a; (cout << ... << (cout << ' ', b)); cout << '\n'; }
+template<class T> void output(vector<T> v) { for (auto u : v) cout << u << ' '; cout << '\n'; };
+void yesno(bool is_ok) { cout << (is_ok ? "yes" : "no") << '\n'; }
+void YesNo(bool is_ok) { cout << (is_ok ? "Yes" : "No") << '\n'; }
+void YESNO(bool is_ok) { cout << (is_ok ? "YES" : "NO") << '\n'; }
+
+// clang-format on
+int dist[61][61][61][61];
+
+const int dy[] = {-1, 0, 1, 0};
+const int dx[] = {0, 1, 0, -1};
+
+int main() {
+    int n;
+    cin >> n;
+    vector<string> s(n);
+    REP(i, n) cin >> s[i];
+    REP(a, n) REP(b, n) REP(c, n) REP(d, n) { dist[a][b][c][d] = INF32; }
+    queue<pair<pair<int, int>, pair<int, int>>> q;
+    {
+        int a, b, c, d;
+        bool is_first = true;
+        REP(i, n) {
+            REP(j, n) {
+                if (s[i][j] == 'P') {
+                    if (is_first) {
+                        a = i, b = j;
+                        is_first = false;
+                    } else {
+                        c = i, d = j;
+                    }
+                }
+            }
+        }
+        q.push({{a, b}, {c, d}});
+        dist[a][b][c][d] = 0;
+    }
+    while (!q.empty()) {
+        auto [x, y] = q.front();
+        q.pop();
+        auto [a, b] = x;
+        auto [c, d] = y;
+        REP(i, 4) {
+            int na = a + dy[i];
+            int nb = b + dx[i];
+            if (na < 0 || nb < 0 || na >= n || nb >= n) {
+                na = a;
+                nb = b;
+            }
+            if (s[na][nb] == '#') {
+                na = a;
+                nb = b;
+            }
+            int nc = c + dy[i];
+            int nd = d + dx[i];
+            if (nc < 0 || nd < 0 || nc >= n || nd >= n) {
+                nc = c;
+                nd = d;
+            }
+            if (s[nc][nd] == '#') {
+                nc = c;
+                nd = d;
+            }
+            if (chmin(dist[na][nb][nc][nd], dist[a][b][c][d] + 1)) {
+                q.push({{na, nb}, {nc, nd}});
+            }
+        }
+    }
+    int ans = INF32;
+    REP(i, n) {
+        REP(j, n) { chmin(ans, dist[i][j][i][j]); }
+    }
+    output(ans == INF32 ? -1 : ans);
+}
