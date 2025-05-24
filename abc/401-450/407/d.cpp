cd97c6ae6eddcebbd076ad1a8fd0d2ecb804c0fd
noko206 <61347127+noko206@users.noreply.github.com>
2025-05-24 14:58:47 +0000

    abc

---
+++ abc/401-450/407/d.cpp
@@ -0,0 +1,71 @@
+// clang-format off
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+#include <atcoder/all>
+using namespace atcoder;
+#endif
+
+#define _overload3(_1,_2,_3,name,...) name
+#define _REP(i,n) REPI(i,0,n)
+#define REPI(i,a,b) for(int i=int(a);i<int(b);++i)
+#define REP(...) _overload3(__VA_ARGS__,REPI,_REP,)(__VA_ARGS__)
+#define _RREP(i,n) RREPI(i,n,0)
+#define RREPI(i,a,b) for(int i=int(a);i>=int(b);--i)
+#define RREP(...) _overload3(__VA_ARGS__,RREPI,_RREP,)(__VA_ARGS__)
+#define ALL(a) (a).begin(),(a).end()
+#define ALLR(a) (a).rbegin(),(a).rend()
+typedef long long ll;
+const int INF32 = 1001001001;
+const long long INF64 = 1001001001001001001;
+struct Init { Init() { ios::sync_with_stdio(0); cin.tie(0); cout << setprecision(15); }} init;
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> T gcd(T x, T y){ return (x % y) ? gcd(y, x % y) : y; }
+template<class T> T lcm(T x, T y){ return x / gcd(x, y) * y; }
+template<class T, class... Ts> void output(const T& a, const Ts&... b) { cout << a; (cout << ... << (cout << ' ', b)); cout << '\n'; }
+template<class T> void output(vector<T> v) { for (auto u : v) cout << u << ' '; cout << '\n'; };
+void yesno(bool is_ok) { cout << (is_ok ? "yes" : "no") << '\n'; }
+void YesNo(bool is_ok) { cout << (is_ok ? "Yes" : "No") << '\n'; }
+void YESNO(bool is_ok) { cout << (is_ok ? "YES" : "NO") << '\n'; }
+
+// clang-format on
+int main() {
+    int h, w;
+    cin >> h >> w;
+    vector a(h, vector<ll>(w));
+    REP(i, h) REP(j, w) cin >> a[i][j];
+    int n = h * w;
+    ll ans = 0;
+    vector put(h, vector<bool>(w, false));
+    auto dfs = [&](auto &dfs, int num = 0) -> void {
+        if (num == n) {
+            ll sum = 0;
+            REP(i, h) REP(j, w) if (!put[i][j]) sum ^= a[i][j];
+            chmax(ans, sum);
+            return;
+        }
+        int r = num / w;
+        int c = num % w;
+        // (i,j) (i,j+1)
+        if (c + 1 != w && !put[r][c] && !put[r][c + 1]) {
+            put[r][c] = true;
+            put[r][c + 1] = true;
+            dfs(dfs, num + 1);
+            put[r][c] = false;
+            put[r][c + 1] = false;
+        }
+        // (i+1,j) (i,j+1)
+        if (r + 1 != h && !put[r][c] && !put[r + 1][c]) {
+            put[r][c] = true;
+            put[r + 1][c] = true;
+            dfs(dfs, num + 1);
+            put[r][c] = false;
+            put[r + 1][c] = false;
+        }
+        // 何も置かない
+        dfs(dfs, num + 1);
+    };
+    dfs(dfs);
+    output(ans);
+}
