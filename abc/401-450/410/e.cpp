c6fa0409197cb9ee37f52630b5250a222c41f94f
noko206 <61347127+noko206@users.noreply.github.com>
2025-06-14 14:29:51 +0000

    ABC410

---
+++ abc/401-450/410/e.cpp
@@ -0,0 +1,59 @@
+// clang-format off
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+#include <atcoder/all>
+using namespace atcoder;
+#endif
+
+#define _overload3(_1,_2,_3,name,...) name
+#define _REP(i,n) REPI(i,0,n)
+#define REPI(i,a,b) for(int i=int(a);i<int(b);++i)
+#define REP(...) _overload3(__VA_ARGS__,REPI,_REP,)(__VA_ARGS__)
+#define _RREP(i,n) RREPI(i,n,0)
+#define RREPI(i,a,b) for(int i=int(a);i>=int(b);--i)
+#define RREP(...) _overload3(__VA_ARGS__,RREPI,_RREP,)(__VA_ARGS__)
+#define ALL(a) (a).begin(),(a).end()
+#define ALLR(a) (a).rbegin(),(a).rend()
+typedef long long ll;
+const int INF32 = 1001001001;
+const long long INF64 = 1001001001001001001;
+struct Init { Init() { ios::sync_with_stdio(0); cin.tie(0); cout << setprecision(15); }} init;
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> T gcd(T x, T y){ return (x % y) ? gcd(y, x % y) : y; }
+template<class T> T lcm(T x, T y){ return x / gcd(x, y) * y; }
+template<class T, class... Ts> void output(const T& a, const Ts&... b) { cout << a; (cout << ... << (cout << ' ', b)); cout << '\n'; }
+template<class T> void output(vector<T> v) { for (auto u : v) cout << u << ' '; cout << '\n'; };
+void yesno(bool is_ok) { cout << (is_ok ? "yes" : "no") << '\n'; }
+void YesNo(bool is_ok) { cout << (is_ok ? "Yes" : "No") << '\n'; }
+void YESNO(bool is_ok) { cout << (is_ok ? "YES" : "NO") << '\n'; }
+
+// clang-format on
+int main() {
+    int n, h, m;
+    cin >> n >> h >> m;
+    vector<int> a(n), b(n);
+    REP(i, n) cin >> a[i] >> b[i];
+    vector dp(n + 1, vector<int>(h + 1, -1));
+    dp[0][h] = m;
+    REP(i, n) {
+        REP(j, h + 1) {
+            if (j + a[i] <= h) {
+                chmax(dp[i + 1][j], dp[i][j + a[i]]);
+            }
+            if (dp[i][j] - b[i] >= 0) {
+                chmax(dp[i + 1][j], dp[i][j] - b[i]);
+            }
+        }
+    }
+    RREP(i, n) {
+        REP(j, h + 1) {
+            if (dp[i][j] >= 0) {
+                output(i);
+                return 0;
+            }
+        }
+    }
+    assert(false);
+}
