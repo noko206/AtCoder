67a05756219062bb53bfbfb1b87a4a286cede0af
noko206 <61347127+noko206@users.noreply.github.com>
2025-09-14 14:20:19 +0000

    ABC

---
+++ abc/401-450/423/e.cpp
@@ -0,0 +1,55 @@
+// clang-format off
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+#include <atcoder/all>
+using namespace atcoder;
+#endif
+
+#define _overload3(_1,_2,_3,name,...) name
+#define _REP(i,n) REPI(i,0,n)
+#define REPI(i,a,b) for(int i=int(a);i<int(b);++i)
+#define REP(...) _overload3(__VA_ARGS__,REPI,_REP,)(__VA_ARGS__)
+#define _RREP(i,n) RREPI(i,n,0)
+#define RREPI(i,a,b) for(int i=int(a);i>=int(b);--i)
+#define RREP(...) _overload3(__VA_ARGS__,RREPI,_RREP,)(__VA_ARGS__)
+#define ALL(a) (a).begin(),(a).end()
+#define ALLR(a) (a).rbegin(),(a).rend()
+typedef long long ll;
+const int INF32 = 1001001001;
+const long long INF64 = 1001001001001001001;
+struct Init { Init() { ios::sync_with_stdio(0); cin.tie(0); cout << setprecision(15); }} init;
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> T gcd(T x, T y){ return (x % y) ? gcd(y, x % y) : y; }
+template<class T> T lcm(T x, T y){ return x / gcd(x, y) * y; }
+template<class T, class... Ts> void output(const T& a, const Ts&... b) { cout << a; (cout << ... << (cout << ' ', b)); cout << '\n'; }
+template<class T> void output(vector<T> v) { for (auto u : v) cout << u << ' '; cout << '\n'; };
+void yesno(bool is_ok) { cout << (is_ok ? "yes" : "no") << '\n'; }
+void YesNo(bool is_ok) { cout << (is_ok ? "Yes" : "No") << '\n'; }
+void YESNO(bool is_ok) { cout << (is_ok ? "YES" : "NO") << '\n'; }
+
+// clang-format on
+int main() {
+    int n, q;
+    cin >> n >> q;
+    vector<ll> a(n);
+    REP(i, n) cin >> a[i];
+    fenwick_tree<ll> ft0(n), ft1(n), ft2(n);
+    REP(i, n) {
+        ft0.add(i, a[i]);
+        ft1.add(i, a[i] * (i + 1));
+        ft2.add(i, a[i] * (ll)(i + 1) * (i + 1));
+    }
+    vector<ll> ans(q);
+    REP(i, q) {
+        ll l, r;
+        cin >> l >> r;
+        ll sum = 0;
+        sum += ft0.sum(l - 1, r) * (-l + 1) * (r + 1);
+        sum += ft1.sum(l - 1, r) * (l + r);
+        sum += ft2.sum(l - 1, r) * (-1);
+        ans[i] = sum;
+    }
+    REP(i, q) output(ans[i]);
+}
