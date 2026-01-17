f740352d9f52d108b07e98306b563ba2697483f6
noko206 <61347127+noko206@users.noreply.github.com>
2026-01-17 17:25:07 +0000

    a

---
+++ abc/401-450/441/d.cpp
@@ -0,0 +1,61 @@
+// clang-format off
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+#include <atcoder/all>
+using namespace atcoder;
+#endif
+
+#define _overload3(_1,_2,_3,name,...) name
+#define _REP(i,n) REPI(i,0,n)
+#define REPI(i,a,b) for(int i=int(a);i<int(b);++i)
+#define REP(...) _overload3(__VA_ARGS__,REPI,_REP,)(__VA_ARGS__)
+#define _RREP(i,n) RREPI(i,n,0)
+#define RREPI(i,a,b) for(int i=int(a);i>=int(b);--i)
+#define RREP(...) _overload3(__VA_ARGS__,RREPI,_RREP,)(__VA_ARGS__)
+#define ALL(a) (a).begin(),(a).end()
+#define ALLR(a) (a).rbegin(),(a).rend()
+typedef long long ll;
+const int INF32 = 1001001001;
+const long long INF64 = 1001001001001001001;
+struct Init { Init() { ios::sync_with_stdio(0); cin.tie(0); cout << setprecision(15); }} init;
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> T gcd(T x, T y){ return (x % y) ? gcd(y, x % y) : y; }
+template<class T> T lcm(T x, T y){ return x / gcd(x, y) * y; }
+template<class T, class... Ts> void output(const T& a, const Ts&... b) { cout << a; (cout << ... << (cout << ' ', b)); cout << '\n'; }
+template<class T> void output(vector<T> v) { for (auto u : v) cout << u << ' '; cout << '\n'; };
+void yesno(bool is_ok) { cout << (is_ok ? "yes" : "no") << '\n'; }
+void YesNo(bool is_ok) { cout << (is_ok ? "Yes" : "No") << '\n'; }
+void YESNO(bool is_ok) { cout << (is_ok ? "YES" : "NO") << '\n'; }
+
+// clang-format on
+int main() {
+    int n, m, l, s, t;
+    cin >> n >> m >> l >> s >> t;
+    vector<vector<pair<int, int>>> to(n);
+    REP(i, m) {
+        int u, v, c;
+        cin >> u >> v >> c;
+        --u, --v;
+        to[u].emplace_back(v, c);
+    }
+    vector<bool> ok(n, false);
+    auto dfs = [&](auto& dfs, int v, ll sum = 0, int cnt = 0) -> void {
+        if (cnt == l) {
+            if (s <= sum && sum <= t) {
+                ok[v] = true;
+            }
+            return;
+        }
+        for (auto [u, c] : to[v]) {
+            dfs(dfs, u, sum + c, cnt + 1);
+        }
+    };
+    dfs(dfs, 0);
+    vector<int> ans;
+    REP(i, n) {
+        if (ok[i]) ans.push_back(i + 1);
+    }
+    output(ans);
+}
